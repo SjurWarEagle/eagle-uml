35a849d27ebbe9ccc1d7f6371edc76905fc6e504
Torsten Kunkel <git@tkunkel.de>
2021-11-20 13:37:50 +0100

    build + proxy conf

--- docker/scripts/start_all.sh
+++ docker/scripts/start_all.sh
@@ -1,5 +1,4 @@
-#nginx -g daemon off
 service nginx restart
-cd /project/server/dist/src
+cd /project/server/dist/
 #TODO do not use src folder build build, that requires less space and tools
 node main
